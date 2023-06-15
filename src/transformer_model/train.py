d121da189d9fb1d8256aaa891c4bc7a2e892f99c
AmineSaidi-ISTIC <59051511+AmineSaidi-ISTIC@users.noreply.github.com>
2023-06-15 10:15:24 +0100

    Update train.py

--- src/transformer_model/train.py
+++ src/transformer_model/train.py
@@ -78,7 +78,7 @@ def train_loop(data_loader, model, criterion_ctc, criterion_transformer, optimiz
         losses['ctc'].update(loss_ctc.item())
         losses['transformer'].update(loss_transformer.item())
         loss.backward()
-        print('data num : '+ str(index) +'/'+ len(data_loader) +' total loss : '+str(losses['total'].avg)+ ' ctc loss : '+str(losses['ctc'].avg)+' transformer loss : '+str(losses['transformer'].avg))
+        print('data num : '+ str(index) +'/'+' total loss : '+str(losses['total'].avg)+ ' ctc loss : '+str(losses['ctc'].avg)+' transformer loss : '+str(losses['transformer'].avg))
         torch.nn.utils.clip_grad_norm_(model.parameters(), 2)
         optimizer.step()
 
